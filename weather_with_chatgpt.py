590214f6a0cfadc108f116645988b70a7947088b
n0-n4-m3 <556sueubaev@mail.ru>
2023-03-28 12:02:25 +0300

    update api keys

--- weather_with_chatgpt.py
+++ weather_with_chatgpt.py
@@ -2,7 +2,7 @@ import openai
 import calendar
 
 def weather(data,event,q):#(dp,user_id):
-    openai.api_key = "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"
+    openai.api_key = "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"
     # data = await dp.storage.get_data(user_id)
     print(data)
     prompt = "кратко предоставь среднестатистическую информацию о погоде в {} в {} на основе доступных данных, не говоря о том что ты 'искуственный интелект' или 'ИИ' или 'бот'".format(data["GEO"]["city"],calendar.month_name[int(data["TIME"]["month"])])
@@ -14,7 +14,7 @@ def weather(data,event,q):#(dp,user_id):
     # return completion
 
 def local_food(data,event,q):
-    openai.api_key = "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"
+    openai.api_key = "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"
     # data = await dp.storage.get_data(user_id)
     print(data)
     prompt = "Выдай список по номерам трёх блюд характерных для местной кухни {} с пояснениями. Напиши менее 1024 символов в формате: 1. Первое блюдо 2. Второе блюдо 3. Третье блюдо   ".format(data["GEO"]["city"])
@@ -27,7 +27,7 @@ def local_food(data,event,q):
     event.set()
 
 def facts(data,event,q):
-    openai.api_key = "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"
+    openai.api_key = "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"
     # data = await dp.storage.get_data(user_id)
     print(data)
     prompt = "расскажи три интересных факта про {} в формате: 1. Первый факт 2. Второй факт 3. Третий факт".format(data["GEO"]["city"])
