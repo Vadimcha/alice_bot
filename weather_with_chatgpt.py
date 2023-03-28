77d242cfcc3c79165808bdcc222849ef5bd39222
n0n4m3 <556sueubaev@mail.ru>
2023-03-28 08:58:47 +0300

    Add housing parsing

--- weather_with_chatgpt.py
+++ weather_with_chatgpt.py
@@ -2,7 +2,7 @@ import openai
 import calendar
 
 def weather(data,event,q):#(dp,user_id):
-    openai.api_key = "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"
+    openai.api_key = "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"
     # data = await dp.storage.get_data(user_id)
     print(data)
     prompt = "кратко предоставь среднестатистическую информацию о погоде в {} в {} на основе доступных данных, не говоря о том что ты 'искуственный интелект' или 'ИИ' или 'бот'".format(data["GEO"]["city"],calendar.month_name[int(data["TIME"]["month"])])
@@ -13,14 +13,17 @@ def weather(data,event,q):#(dp,user_id):
     event.set()
     # return completion
 
-def places(data,event,q):
-    openai.api_key = "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"
+def local_food(data,event,q):
+    openai.api_key = "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"
     # data = await dp.storage.get_data(user_id)
     print(data)
-    prompt = "Выдай список интерсных мест для посещения в {} без предисловия и лишнего текста".format(data["GEO"]["city"])
+    prompt = "Выдай список по номерам трёх блюд характерных для местной кухни {} с пояснениями. Напиши менее 1024 символов".format(data["GEO"]["city"])
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])["choices"][0]["message"]["content"]
     # await dp.storage.update_data(user_id,weather=completion)
     print("я всё x2")
+    print(completion)
+    j = completion.find('1')
+    completion = completion[j:]
     q.put(completion)
     event.set()
 
